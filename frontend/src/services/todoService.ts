3b83dce7d9fa9316c0a5f71da43257dca00434e3
AlexTrejo <alextrejores@gmail.com>
2022-09-13 01:39:23 -0500

    Added Service CRUD methods

---
+++ frontend/src/services/todoService.ts
@@ -0,0 +1,42 @@
+const baseUrl = `${process.env.REACT_APP_API_URL}/todos`;
+
+export const loadTodos = () => {
+    return fetch(baseUrl).then((res) => res.json());
+}
+
+export const getTodos = (id: string ) => {
+    return fetch(`${baseUrl}/${id}`).then((res) => res.json());
+}
+
+export const createTodo = (todo: any) => {
+    return fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: todo.title,
+            completed: todo.completed
+        }),
+    }).then((res) => res.json());
+}
+
+export const updateTodo = (todo: any) => {
+    return fetch(`${baseUrl}/${todo.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: todo.id,
+            title: todo.title,
+            completed: todo.completed
+        }),
+    }).then((res) => res.json());
+}
+
+export const deleteTodo = (id: string ) => {
+    return fetch(`${baseUrl}/${id}`, {
+        method: "DELETE",
+    }).then((res) => res.json());
+}

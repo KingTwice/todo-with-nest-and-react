f870c751b8f22052284535e68af8acc339adb513
AlexTrejo <alextrejores@gmail.com>
2022-09-13 01:13:47 -0500

    Added Frontend app and moved backend to Backend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.min.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
